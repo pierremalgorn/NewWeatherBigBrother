0d2342ba6502b316ceddc7af3973ce71cb37bba8
Pierre <pierre.malgorn@sfr.fr>
2015-04-21 18:06:55 +0200

    Finitions Pierre

--- js/custom.js
+++ js/custom.js
@@ -5,53 +5,29 @@ jQuery(document).ready(function($) {
         types: ['(cities)']
     });
 	
-	//Script de slide gauche/droite
-	var query = window.location.href;
-	var vars = query.split("#");
-	var flag = 0;
-	(vars[1] >= 0 && vars[1] <= $(".nav li").length) ? goTo(vars[1]) : goTo(0);
-	$( ".nav li" ).click(function() {
-		if(flag == 1) {return false;}
-		$(".nav li").removeClass("active");
-		$(this).addClass("active")
-		flag = 1;
-		$( "#slider" ).animate({ "margin-left": -($(this).index() * $("body").width()) + "px" }, "slow", function(){
-			flag = 0;
-		});
-		menu_focus(this);
-	});
-		
-		
-	function goTo(nb){
-			$( "#slider" ).css( "margin-left", - (nb * $("body").width()) +"px");
-			$(".nav li").removeClass("active");
-			$('.nav li:nth('+nb+')').addClass("active");
-			
-	}
-	});
+//Script de slide gauche/droite
+var flag = 0;
+var slideHeight;
 
-function menu_focus( element) {
-	/*if ( $(element).hasClass('iactive') || $(element).data("slide") == 0) {
-		console.log($(element).data("slide"));
-		console.log(element);
-			return;
-	}*/
+//Au clic sur un élément du menu
+$( ".nav li" ).click(function() {  
+    if(flag == 1) {return false;} //On bloque le mouvement  
+    $(".nav li").removeClass("active");
+    $(this).addClass("active")
+    flag = 1;
+    slideHeight = $(".pageSlide").eq($(this).index()).height();
+    $("#wrapperHidden").css("height", slideHeight);
+    $( "#slider" ).animate({ "margin-left": -($(this).index() * $("body").width()) + "px" }, "slow", function(){
+            flag = 0;
+    });
+});
 
-console.log(element);
-	$('.nav > li').removeClass('active');
-	$(element).addClass('active');
-	
-	var icon = $(element).find('.glyphicon');
-	
-	var left_pos = $('.nav').offset().left;
-	var el_width = $(element).width();
-	
-	$('.active-menu').stop(false, false).animate(
-		{
-			left: left_pos,
-			width: el_width
-		},
-		1500,
-		'easeInOutQuart'
-	);
+
+function goTo(nb){
+                $( "#slider" ).css( "margin-left", - (nb * $("body").width()) +"px");
+                $(".nav li").removeClass("active");
+                $('.nav li:nth('+nb+')').addClass("active");
+
 }
+    
+});
